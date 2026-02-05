3ccc06eaea143b0d6706feb501d41511245a0770
AyaAhmed84 <ayaahmed080402004@gmail.com>
2026-02-05 16:18:26 +0200

    add full rescufy backend project

---
+++ backend/RESCUFY.API/RESCUFY.API/DTOs/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RESCUFY.API.DTOs
+{
+    public class RegisterDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        public int RoleID {  get; set; }
+    }
+}
